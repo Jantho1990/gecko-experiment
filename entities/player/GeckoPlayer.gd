756ad1b03f4da53dc9656f9a2ded1fe65e2855ad
Joshua Anthony <jantho1990@gmail.com>
2019-05-27 13:47:11 -0500

    added more to BoxesAndLedges test map; worked on fixing bugs with gecko grip

--- entities/player/GeckoPlayer.gd
+++ entities/player/GeckoPlayer.gd
@@ -7,6 +7,12 @@ extends "res://entities/player/Player.gd"
 var currently_gripping = false
 var should_grip = false
 
+# If true, entity is transferring from wall to ceiling,
+# or vice versa.
+var grip_transfer_in_progress = false
+var in_corner = false
+var grip_transfer_rotation_angle = 0
+
 # Which side of the entity is doing the gripping.
 var grip_direction = Vector2(0, 0)
 
@@ -28,8 +34,9 @@ func _ready():
 	}
 
 func _physics_process(delta):
+	print(motion.y)
 	should_grip = false
-	if Input.is_action_just_pressed("ui_grip"):
+	if Input.is_action_pressed("ui_grip"):
 		if not currently_gripping:
 			should_grip = true
 		else:
@@ -37,9 +44,19 @@ func _physics_process(delta):
 			grip_direction = Vector2(0, 0)
 			gravity = true
 			$MovementHandler.clear_overrides()
-			
-		
-	if is_on_wall() or is_on_ceiling():
+	
+	if grip_transfer_in_progress:
+#		rotate_to_ceiling()
+		pass
+	
+	if is_on_wall() and is_on_ceiling():
+		if not in_corner:
+			grip_transfer_in_progress = true
+			in_corner = true
+		else:
+			print("Still in corner.")
+	elif is_on_wall() or is_on_ceiling():
+		in_corner = false
 		if should_grip and not currently_gripping:
 			motion.y = 0
 			currently_gripping = true
@@ -47,9 +64,11 @@ func _physics_process(delta):
 			gravity = false
 			$MovementHandler.set_overrides(movement_overrides)
 	else:
+		in_corner = false
 		currently_gripping = false
 	
 	if currently_gripping:
+		print("gripping...", grip_direction)
 		if Input.is_action_pressed("ui_down"):
 			$MovementHandler.down()
 		elif Input.is_action_pressed("ui_up"):
@@ -68,8 +87,10 @@ func apply_motion():
 		.apply_motion()
 	else:
 		# apply a smidgen of force to trigger is_on_whatever calculations
-		motion += 7.015 * grip_direction # I have no idea why 7.015 is the minimum needed force for this to work, I just trial-and-error'd it.
+		motion += 527.015 * grip_direction # I have no idea why 7.015 is the minimum needed force for this to work, I just trial-and-error'd it.
+		print("premotion", motion, grip_direction)
 		motion = move_and_slide(motion, UP)
+		print(motion, grip_direction)
 
 func calculate_grip_direction(wall_normal): # Translate the wall_normal into grip direction, which matches our regular direction's convention.
 	grip_direction -= wall_normal
@@ -110,6 +131,9 @@ func grip_move_right():
 
 func grip_move_up():
 	direction.y = -1
+	if grip_direction.y + direction.y == 0:
+		return
+	
 	motion.y = max(motion.y - ACCELERATION, -MAX_SPEED)
 
 func get_height():
@@ -117,3 +141,22 @@ func get_height():
 
 func get_width():
 	return $CollisionShape2D.shape.width
+
+func in_midair(delta):
+	.in_midair(delta)
+	if not in_corner and abs(rotation_degrees) > 0:
+		rotation_degrees += sign(rotation_degrees) * lerp(rotation_degrees, 0, 0.5)
+
+func rotate_to_ceiling():
+	currently_gripping = true # Force this to stay true, since the rotation will throw it off.
+	var dir = direction.x
+	grip_transfer_rotation_angle += 5 * -dir
+	rotation_degrees = grip_transfer_rotation_angle
+	motion.y -= 500
+	print("Rotation: ", grip_transfer_rotation_angle, " ", dir)
+	if abs(grip_transfer_rotation_angle) >= 90:
+		print("sHOT")
+		grip_transfer_in_progress = false
+		grip_transfer_rotation_angle = 0
+		if not is_on_ceiling():
+			position.y -= 500
