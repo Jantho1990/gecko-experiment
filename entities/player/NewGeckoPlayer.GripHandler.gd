c848c03719f07e951a85d2a4324cb5dd567f0547
Joshua Anthony <jantho1990@gmail.com>
2019-06-08 09:10:36 -0500

    added GripDetection system; refactored Player to use the GripDetection system for grip checks

---
+++ entities/player/NewGeckoPlayer.GripHandler.gd
@@ -0,0 +1,195 @@
+extends "res://entities/player/Player.gd"
+
+###
+# Controls whether the player is currently gripping a wall or ceiling
+# and if they should be doing that.
+###
+var grip_pressed = false
+var grip_releasing = false
+var grip_hold = true
+var gripping_surface = false
+
+
+
+# Are we in a corner right now?
+var in_corner = false
+var corner_direction = Vector2(0, 0) # used to save the grip direction at the time a corner is being navigated
+
+var movement_overrides setget _set_private,_get_private
+
+onready var GripDetection = $GripDetection
+
+func _set_private(_throwaway_):
+	print("Setting not allowed.")
+
+func _get_private():
+	print("Getting not allowed.")
+
+func _ready():
+	movement_overrides = {
+		"down": funcref(self, "grip_move_down"),
+		"idle": funcref(self, "grip_move_idle"),
+		"left": funcref(self, "grip_move_left"),
+		"right": funcref(self, "grip_move_right"),
+		"up": funcref(self, "grip_move_up")
+	}
+	GripDetection.set_dimensions(get_width(), get_height())
+
+func _physics_process(delta):
+	update() # Used to trigger the _draw function
+	
+	if Input.is_action_pressed("ui_grip"):
+		grip_pressed = true
+		
+	if Input.is_action_just_released("ui_grip"):
+		grip_pressed = false
+		grip_releasing = false
+	
+	if should_attempt_grip():
+		GripDetection.scan_for_grippable_surfaces()
+		if GripDetection.near_grippable_surface():
+			grip_surface()
+	elif should_release_grip():
+		release_grip()
+	elif grip_releasing:
+		handle_release_cancel()
+	
+	if gripping_surface:
+		handle_grip_movement()
+
+
+###
+# Other Methods
+###
+
+func should_attempt_grip():
+	return grip_pressed and not gripping_surface and not grip_releasing
+
+func should_release_grip():
+	return (grip_releasing or Input.is_action_just_pressed("ui_grip")) and gripping_surface
+
+func handle_release_cancel():
+	GripDetection.scan_for_grippable_surfaces()
+	if not GripDetection.near_grippable_surface():
+		grip_releasing = false
+
+func grip_surface():
+	if not gripping_surface and GripDetection.grippable_surface_contact():
+		gripping_surface = true
+		gravity = false
+		$MovementHandler.set_overrides(movement_overrides)
+	else:
+		motion += 500 * GripDetection.grip_direction
+
+func release_grip():
+	gripping_surface = false
+	if in_corner:
+		in_corner = false
+	if not grip_releasing:
+		grip_releasing = true
+	gravity = true
+	$MovementHandler.clear_overrides()
+
+func handle_grip_movement():
+	if Input.is_action_pressed("ui_down"):
+		$MovementHandler.down()
+	elif Input.is_action_pressed("ui_up"):
+		$MovementHandler.up()
+	else:
+		$MovementHandler.idle()
+	
+	# Update gripping position
+	GripDetection.scan_for_grippable_surfaces()
+	if not GripDetection.near_grippable_surface():
+		grip_releasing = true
+	
+	# Corner detection
+	if GripDetection.in_obtuse_angle_corner():
+		obtuse_angle_corner_transfer()
+		print("obtuse")
+	elif GripDetection.in_reflex_angle_corner():
+		reflex_angle_corner_transfer()
+		print("reflex")
+	else:
+#		print("nah")
+		pass
+
+func obtuse_angle_corner_transfer():
+	if not in_corner:
+		in_corner = true
+		corner_direction = GripDetection.grip_direction
+	rotation_degrees += 0.1 * (-corner_direction.x if corner_direction.x != 0 else -corner_direction.y)
+	motion += 500 * corner_direction
+	
+	if GripDetection.grippable_surface_hit("left") and \
+		GripDetection.grippable_surface_contact("left"):
+			breakpoint
+	else:
+		print("left ", GripDetection.grippable_surface["left"], position)
+	if GripDetection.grippable_surface_hit("right") and \
+		GripDetection.grippable_surface_contact("right"):
+			breakpoint
+	elif GripDetection.grippable_surface_hit("right"):
+		print("right ", GripDetection.grippable_surface["right"], position)
+#	else:
+#		print(grip_direction, position)
+#		breakpoint
+
+func reflex_angle_corner_transfer():
+	pass
+
+func grip_move_down():
+	direction.y = 1
+	if GripDetection.grip_direction.y + direction.y == 0:
+		return
+		
+	motion.y = min(motion.y + ACCELERATION, MAX_SPEED)
+
+func grip_move_idle():
+	friction = true
+	if motion.y > 0:
+		motion.y = max(motion.y - ACCELERATION, 0)
+	elif motion.y < 0:
+		motion.y = min(motion.y + ACCELERATION, 0)
+	
+	# We already have horizontal friction handling in base move_idle,
+	# so let's steal it.
+	.move_idle()
+
+func grip_move_left():
+	direction.x = -1
+	if GripDetection.grip_direction.x + direction.x == 0:
+		return
+	
+	motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+	
+	playAnim('run', -1, 1.6)
+
+func grip_move_right():
+	direction.x = 1
+	if GripDetection.grip_direction.x + direction.x == 0:
+		return
+	
+	motion.x += ACCELERATION
+	motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+	
+	playAnim('run', -1, 1.6)
+
+func grip_move_up():
+	direction.y = -1
+	if GripDetection.grip_direction.y + direction.y == 0:
+		return
+	
+	motion.y = max(motion.y - ACCELERATION, -MAX_SPEED)
+
+func get_height():
+	return $CollisionShape2D.shape.extents.y * 2
+
+func get_half_height():
+	return $CollisionShape2D.shape.extents.y
+
+func get_width():
+	return $CollisionShape2D.shape.extents.x * 2
+
+func get_half_width():
+	return $CollisionShape2D.shape.extents.x
---
+++ systems/grip_detection/GripDetection.gd
@@ -0,0 +1,247 @@
+extends Node2D
+
+const THRESHOLD = 3
+
+# Which side of the entity is doing the gripping.
+var grip_direction = Vector2(0, 0)
+
+var grip_vectors = {}
+var grippable_surface = {}
+var grip_range = 15
+
+var in_corner = false
+
+var width = 1 # start at 1 to prevent division by 0
+var height = 1 # start at 1 to prevent division by 0
+
+var raycasts = {}
+
+func _ready():
+	pass
+#	grip_vectors = create_grip_vectors()
+#	raycasts = create_raycasts()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		breakpoint
+	calculate_grippable_surfaces()
+	update()
+
+func _draw():
+	draw_grip_range()
+
+func calculate_grip_direction():
+	grip_direction = Vector2(0, 0)
+	for key in grippable_surface:
+		var surface = grippable_surface[key]
+		if surface.hit:
+			match key:
+				"right":
+					grip_direction += Vector2(1, 0)
+				"left":
+					grip_direction += Vector2(-1, 0)
+				"down":
+					grip_direction += Vector2(0, 1)
+				"up":
+					grip_direction += Vector2(0, -1)
+
+func calculate_grippable_surfaces(pos = position):
+	var ret = {}
+	for key in grip_vectors:
+		var grip_vector = grip_vectors[key]
+		var range_value = grip_range * grip_vector.normalized()
+		var raycast = raycasts[key]
+		var result = get_raycast_result(raycast)
+		var hit = !result.empty()
+		ret[key] = {
+			"hit": hit,
+			"grip_vector": grip_vector,
+			"contact": false,
+			"raycast": raycast
+		}
+		if hit:
+#			var distance = result.position.distance_to(raycast.global_position) # need global_position because position just returns this node, which is relative to its parent
+#			breakpoint
+			ret[key] = {
+				"hit": hit,
+				"distance": result.distance,
+				"contact": in_contact_range(result.distance),
+				"result": result,
+				"raycast": raycast,
+				"grip_vector": grip_vector
+			}
+	
+	grippable_surface = ret
+
+func create_grip_vectors():
+	# An array of vectors representing the eight sides
+	# of the entity, which will be used to perform
+	# eight raycasts.
+	# Adding half-dimensions accounts for the dimensions
+	# of the entity so grip detection doesn't start from
+	# the dead center of entity.
+	return {
+		"right": Vector2(get_half_width(), 0), # Right
+		"left": Vector2(-get_half_width(), 0), # Left
+		"down": Vector2(0, get_half_height()), # Down
+		"up": Vector2(0, -get_half_height()), # Up
+		"down-right": Vector2(get_half_width(), get_half_height()), # Down-Right
+		"down-left": Vector2(-get_half_width(), get_half_height()), # Down-Left
+		"up-right": Vector2(get_half_width(), -get_half_height()), # Up-Right
+		"up-left": Vector2(-get_half_width(), -get_half_height()) # Up-Left
+	}
+
+func create_raycast(grip_vector, range_value):
+	var ray = RayCast2D.new()
+	ray.position += grip_vector
+	ray.cast_to = range_value
+	ray.exclude_parent = true
+	ray.collide_with_areas = true
+	ray.collide_with_bodies = true
+	ray.enabled = true
+	ray.add_exception(get_parent())
+	return ray
+
+func create_raycasts():
+	var ret = {}
+	for key in grip_vectors:
+		var grip_vector = grip_vectors[key]
+		var range_value = grip_range * grip_vector.normalized()
+		var raycast = create_raycast(grip_vector, range_value)
+		add_child(raycast)
+		ret[key] = raycast
+	return ret
+
+func draw_grip_range():
+	if not grip_vectors.empty():
+		for surface in grippable_surface.values():
+			var grip_vector = surface.grip_vector
+			var range_value = grip_range * grip_vector.normalized()
+			var raycast = surface.raycast
+#			if grip_pressed:
+#				print("Position: ", position, ", Global Position: ", global_position, ", Grip Vector: ", grip_vector)
+#			draw_circle(Vector2(0, 0), 50, Color(1, 1, 1))
+			var color = Color(1, 0, 0)
+			if surface.contact:
+				color = Color(0, 1, 0)
+			elif surface.hit:
+				color = Color(0, 0, 1)
+			draw_line(raycast.position, raycast.position + raycast.cast_to, color)
+			var ln = raycast.position + raycast.cast_to
+#			draw_line(Vector2(0, 0) + grip_vector, grip_vector + range_value, color, 1)
+
+func get_height():
+	return height
+
+func get_half_height():
+	return height / 2
+
+func get_width():
+	return width
+
+func get_half_width():
+	return width / 2
+
+func get_raycast_result(raycast):
+	raycast.force_raycast_update() # this is needed because the parent has moved since the last calculation, which throws off the distance calculation; feels ugly but the alternative is copying parent motion into the distance calculation, which seems worse
+	if raycast.is_colliding():
+		var distance = raycast.get_collision_point().distance_to(raycast.global_position)
+		return {
+			"position": raycast.get_collision_point(),
+			"collider": raycast.get_collider(),
+			"distance": distance
+		}
+	return {}
+
+func grippable_surface_contact(direction = ""):
+	if direction == "":
+		for surface in grippable_surface.values():
+			if surface.hit:
+				return true
+	
+	if grippable_surface.has(direction):
+		if grippable_surface[direction].contact:
+			return true
+	else:
+		print("Unknown grip direction: ", direction)
+	
+	return false
+
+func grippable_surface_hit(direction = ""):
+	if typeof(direction) == TYPE_STRING and direction == "":
+		for surface in grippable_surface.values():
+			if surface.hit:
+				return true
+	elif typeof(direction) == TYPE_ARRAY:
+		for dir in direction:
+			if grippable_surface.has(dir):
+				if grippable_surface[dir].hit:
+					return true
+			else:
+				print("Unknown grip direction: ", dir)
+	elif grippable_surface.has(direction):
+		if grippable_surface[direction].hit:
+			return true
+	else:
+		print("Unknown grip direction: ", direction)
+	
+	return false
+
+func grippable_surface_hit_all(direction = ""):
+	if typeof(direction) == TYPE_STRING and direction == "":
+		return grippable_surface_hit(direction)
+	elif typeof(direction) == TYPE_ARRAY:
+		for dir in direction:
+			var ret = grippable_surface_hit(dir)
+			if not ret:
+				return false
+		return true
+
+func in_contact_range(distance):
+	return distance <= THRESHOLD
+
+# Corner detection
+# For obtuse-angle corners ( 90 < angle < 180):
+# - rotate away from current surface
+# - push entity up towards target surface
+# - this should reduce the distance of the mid-raycast for the gripping side
+# - when the gripping side raycast approaches or reaches zero
+#   - snap rotation to uniform angle
+#   - end push
+# For reflex-angle corners ( angle > 180deg):
+# - rotate towards current surface, which should also rotate towards target surface
+# - everything else is the same as for acute-angle corners
+# Threshold should prolly be < 0.01
+# - this would definitely require rotation snap
+
+func in_obtuse_angle_corner():
+	return in_corner or grippable_surface_hit(["up", "down"])
+
+func in_reflex_angle_corner():
+	return not grippable_surface_hit(["left", "right"])
+
+func near_grippable_surface():
+	for surface in grippable_surface.values():
+		if surface.hit:
+			return true
+	return false
+
+func scan_for_grippable_surfaces():
+	calculate_grippable_surfaces()
+	calculate_grip_direction()
+
+func set_dimensions(width, height):
+	self.width = width
+	self.height = height
+	grip_vectors = create_grip_vectors()
+	raycasts = create_raycasts()
+
+func surface_in_contact_range(direction = null):
+	if direction == null:
+		return in_contact_range(direction.distance)
+	else:
+		for surface in grippable_surface.values():
+			if in_contact_range(surface.distance):
+				return true
+	
+	return false
